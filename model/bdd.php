eae399b73f720a096ff2dba033d5c6ea8d091887
Auguste Laulhau <aug.laulhau@gmail.com>
2014-11-03 10:53:29 +0100

    modification view modele

--- model/bdd.php
+++ model/bdd.php
@@ -1,7 +1,7 @@
 <?php 
 		try
 		{
-			$db = new PDO('mysql:host=localhost;dbname=seeit', 'root','root');
+			$db = new PDO('mysql:host=localhost;dbname=seeit', 'root','');
 			$db->query('SET NAMES utf8');
 		}
 		catch (Exception $e)
