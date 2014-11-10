13f5958b7b359d6cff12570c13bab32f77543f3a
louis charbonier <anibalfps@gmail.com>
2014-11-10 12:22:01 +0100

    Dernier ajout - Layout

--- view/index.php
+++
@@ -1,6 +0,0 @@
-
-
-Prochains trucs à faire : </br>
-- Inscription et connexion en .php et lisaison à la BDD </br>
-- Trouver le plugin .js qui permet d'afficher les photos en 3d </br>
-- Vérifier que slimlayout marche. </br>
