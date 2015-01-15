2c3c3fbd8f4dfeb9af8645a5bd5d686fd3842b79
Guillaume FORGUE <guillaume.forgue@gmail.com>
2015-01-15 11:55:50 +0100

    reindentation classes et fonctions

--- view/authentification/infocompte.php
+++ view/authentification/infocompte.php
@@ -3,22 +3,22 @@
 <?php
 if (!empty($_SESSION['utilisateur_id']))
 {
-?>
-		<div class='row content'>
+	?>
+	<div class='row content'>
 		<div class='small-8 large-8 columns'>
-		<h1 class='columns large-12 center' >VOTRE COMPTE</h1>
-		<div id='infocompte' class='columns small-12 large-12 center'>
-		<strong>Votre nom : </strong><?php echo $this->data["row"]["last_name"];?></br>
-		<strong>Votre prenom : </strong><?php echo $this->data["row"]["first_name"];?></br>
-		<strong>Votre mail : </strong><?php $this->data["row"]["mail"];?></br>
-		</div>
-		<a href='/seeit/modifiercompte' class='button expand' style='margin-top: 68px' >Modification du compte</a>
+			<h1 class='columns large-12 center' >VOTRE COMPTE</h1>
+			<div id='infocompte' class='columns small-12 large-12 center'>
+				<strong>Votre nom : </strong><?php echo $this->data["row"]["last_name"];?></br>
+				<strong>Votre prenom : </strong><?php echo $this->data["row"]["first_name"];?></br>
+				<strong>Votre mail : </strong><?php $this->data["row"]["mail"];?></br>
+			</div>
+			<a href='/seeit/modifiercompte' class='button expand' style='margin-top: 68px' >Modification du compte</a>
 		</div>
 		<div class='small-4 large-4 columns'>
-		<a href='/seeit/modifierimage'><img src='img/<?php echo $this->data["row"]["avatar"];?>' id='photoavatar'/></a>
+			<a href='/seeit/modifierimage'><img src='img/<?php echo $this->data["row"]["avatar"];?>' id='photoavatar'/></a>
 		</div>
-		</div>
-<?php
+	</div>
+	<?php
 }
 else{
 	echo "Vous n'êtes pas inscrit" ;
